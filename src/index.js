42ff8108c96e02c72596eda1b7598f66308e80d3
Ben Arts <Ben.Arts@student.hogent.be>
2021-11-04 15:57:16 +0100

    koa/cors toegevoegd

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ const Koa = require('koa');
 const config = require('config');
 const bodyParser = require('koa-bodyparser');
 const {initializeLogger, getLogger} = require('./core/logging');
+const koaCors = require('@koa/cors');
 
 const installRest = require('./rest');
 
 
 const NODE_ENV = config.get('env');
+const CORS_ORIGINS = config.get('cors.origins');
+const CORS_MAX_AGE = config.get('cors.maxAge');
 const LOG_LEVEL = config.get('log.level');
 const LOG_DISABLED = config.get('log.disabled');
 
@@ -16,7 +19,23 @@ initializeLogger({
 	isProduction: NODE_ENV === 'production',
 	defaultMeta: {NODE_ENV},
 });
+
 const app = new Koa();
+
+app.use(
+	koaCors({
+		origin: (ctx) => {
+			if (CORS_ORIGINS.indexOf(ctx.request.header.origin) !== -1) {
+				return ctx.request.header.origin;
+			}
+			// Not a valid domain at this point, let's return the first valid as we should return a string
+			return CORS_ORIGINS[0];
+		},
+		allowHeaders: ['Accept', 'Content-Type', 'Authorization'],
+		maxAge: CORS_MAX_AGE,
+	})
+);
+
 const logger = getLogger();
 
 app.use(bodyParser());
