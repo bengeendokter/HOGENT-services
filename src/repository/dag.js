ad6bb5ff57b3b47230671b5c4bc0ac8f73341689
Ben Arts <Ben.Arts@student.hogent.be>
2021-11-09 22:15:59 +0100

    complexere knex query

--- src/repository/dag.js
+++ src/repository/dag.js
@@ -5,11 +5,19 @@ const findAll = ({
     offset,
 }) =>
 {
-    return getKnex()(tables.dagen)
-        .select()
+
+
+    return getKnex()
+        .select(`${tables.dagen}.id AS datum`)
+        .count(`${tables.dagenleden}.id AS aanwezig`)
+        .from(tables.dagen)
+        .join(tables.dagenleden, `${tables.dagen}.id`, `${tables.dagenleden}.dagid`)
+        .where(`${tables.dagenleden}.aanwezig`, 1)
+        .groupBy(`${tables.dagen}.id`)
         .limit(limit)
-        .offset(offset)
-        .orderBy('id', 'ASC');
+		.offset(offset)
+
+        // knex.select('year', knex.raw('SUM(profit)')).from('sales').groupByRaw('year WITH ROLLUP')
 };
 
 module.exports = {
