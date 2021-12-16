e8c97271103eb297f90dd3442c0d66c21701fec6
Ben Arts <ben.arts@student.hogent.be>
2021-12-16 15:34:43 +0100

    test config

---
+++ config/test.js
@@ -0,0 +1,32 @@
+module.exports = {
+  log: {
+    level: 'silly',
+    disabled: true,
+  },
+  cors: {
+    origins: ['http://localhost:3000'],
+    maxAge: 3 * 60 * 60,
+  },
+  database: {
+    client: 'mysql2',
+    host: 'localhost',
+    port: 3306,
+  },
+  pagination: {
+    limit: 100,
+    offset: 0,
+  },
+  auth: {
+    argon: {
+      saltLength: 16,
+      hashLength: 32,
+      timeCost: 6,
+      memoryCost: 2 ** 17,
+    },
+    jwt: {
+      expirationInterval: 60 * 60 * 1000, // ms (1 hour)
+			issuer: 'aanwezigheden.app',
+			audience: 'aanwezigheden.app',
+    },
+  },
+};
